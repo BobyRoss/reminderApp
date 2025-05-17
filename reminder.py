0ff6ff355c341f64b8fe9d66d837fd7a4eb7d29e
orelo <sogo.rush@gmail.com>
2025-05-16 19:05:08 -0700

    added readme file

--- reminder.py
+++ reminder.py
@@ -3,24 +3,35 @@ import time
 import subprocess
 import pyautogui
 
+##Initially used to only be able to create one reminder
+#reminder_day = "Friday"
+#reminder_time = "18:32"
+#reminder_text = "Submit Assignment"
 
-reminder_day = "Friday"
-reminder_time = "18:32"
-reminder_text = "Submit Assignment"
+#Create an array of reminders
+reminders = [
+    ["Friday", "22:00", "go to sleep"],
+    ["Saturday", "23:00", "SLEEP!!"],
+    ["Friday", "19:02", "one"],
+    ["Friday", "19:03", "two"],
+]
 
-def waitUntil(day, time_str):
+def waitUntil(r):
     while True:
-        now = datetime.datetime.now()
-        current_day = now.strftime("%A")
-        current_time = now.strftime("%H:%M")
-        if current_day == day and current_time == time_str:
-            return
-        time.sleep(30)
+        for reminder in r:
+            now = datetime.datetime.now()
+            current_day = now.strftime("%A")
+            current_time = now.strftime("%H:%M")
+
+
+            if current_day == reminder[0] and current_time == reminder[1]:
+                show_reminder(reminder[2])    
+        time.sleep(60)
+
 
 def show_reminder(text):
     subprocess.Popen("notepad.exe")
     time.sleep(1.5)
     pyautogui.typewrite(text, interval=0.05)
 
-waitUntil(reminder_day, reminder_time)
-show_reminder(reminder_text)    
+waitUntil(reminders)
